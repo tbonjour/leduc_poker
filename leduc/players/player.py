ff1ce3cb021b6cad7f9c60810fd180a6f4f92f68
Trevor <bonjour.trevor@gmail.com>
2021-04-01 17:17:54 -0400

    Added an attribute to track amount of a player

--- leduc/players/player.py
+++ leduc/players/player.py
@@ -1,14 +1,15 @@
 
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, init_amount=100):
         self.name = name
+        self.amount = init_amount
         self.bets = 1
         self.folded = False
         self.raised = False
 
     def __repr__(self):
-        return self.name
-        # return f"Name:{self.name} Bets: {self.bets} Folded: {self.folded}"
+        # return self.name
+        return f"Name:{self.name} Amount: {self.amount}"
 
     def __eq__(self, other):
         return self.bets == other.bets
